dcbf784cf7b45e6873fe6cfda39fce2a9b07c9e8
siwar-ghouma <siwar.ghouma@sastec-group.com>
2022-05-15 21:21:17 +0200

    prepare login page

---
+++ src/components/Button.jsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import styled from "styled-components";
+
+export default function Button({ content , click }) {
+    useEffect(()=> {console.log("hhhh");},[])
+  return <StyledButton onClick={() =>click()}>{content}</StyledButton>;
+}
+
+const StyledButton = styled.button`
+  background:linear-gradient(to right, #14163c 0%, #03217b 79%) ;
+  text-transform: uppercase;
+  letter-spacing: 0.2rem;
+  width: 65%;
+  height: 3rem;
+  border: none;
+  color: white;
+  border-radius: 2rem;
+  cursor: pointer;
+`;
